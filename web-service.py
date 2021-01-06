4e51c52e5e2df005f48244bac2c87121e64bd050
EvanGreaney <g00348451@gmit.ie>
2021-01-06 21:21:25 +0000

    Webservice taking data from model and power level is being displayed when the user inputs a speed

---
+++ web-service.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
+import json
+from flask import Flask, jsonify,request
+app = Flask(__name__)
+
+@app.route('/')
+def runner():
+    return app.send_static_file('index.html')
+
+@app.route('/home', methods=['GET'])
+def getHome():
+    return app.send_static_file('home.html')
+
+@app.route('/power', methods=['POST'])
+def parse_request():
+    data = request.data
+    returnedData = json.loads(data)
+    windSpeed = float(returnedData['speed'])
+    model = tf.keras.models.load_model('modelpowerproduction.h5')
+    prediction = model.predict([windSpeed])
+    predictionOneDimen = prediction.flatten()
+    print(predictionOneDimen[0])
+    return str(predictionOneDimen[0])
+
